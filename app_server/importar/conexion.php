75108c54fd60416284b626b103319dabff4c9c2e
jose m <josemcarvajal4@gmail.com>
2019-09-20 12:57:53 -0430

    version 5.1
    corregido la conexion

--- app_server/importar/conexion.php
+++ app_server/importar/conexion.php
@@ -6,7 +6,7 @@
 	   
 
 
-	   if (!($cn=pg_connect("host=localhost port=5432 dbname=reatci user=postgres password=kara$$17")))
+	   if (!($cn=pg_connect("host=ec2-23-21-238-246.compute-1.amazonaws.com port=5432 dbname=dtisspb366oho user=osjgkygtrpojcg password=d071b435c9e0023ab89ddc749d10bb44854f28c5f584a504b47d84b43f99fa75")))
 	   {
 		  echo "Error conectando a la base de datos.";
 		  exit();
