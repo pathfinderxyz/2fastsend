b53a0bdb719c43556f01e4d351f085c5e4961e20
pathfinderxyz <josemcarvajal4@gmail.com>
2019-12-12 11:17:08 -0400

    prueba iframe

--- app_server/buscar/tracking_vista.php
+++ app_server/buscar/tracking_vista.php
@@ -1,26 +1,7 @@
-<div class="row">
-     <div class="col-xs-12">
-                     <div class="card">
-                         <div class="card-header">
-                             <div class="card-title">
-                                 <div class="title">Consultar Tracking</div>
-                             </div>
-                         </div>
-                         <div class="card-body">
-                             <h5>Introduzca el numero de guia para consultar su paquete</h5>
-                             <form  method="post" action="?page=buscar" class="form-inline"> 
-                             <div class="form-group">
-                               <input type="text" class="form-control" name="guia" autofocus placeholder="Buscar">
-                             </div>
-                                 
-                             <button class="btn btn-primary" type="submit">
-                                 <span class="glyphicon glyphicon-search"></span>
-                             </button> 
-                             </form>  
-                      
-                         </div>
-                 
-                   </div>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/v64KOxKVLVg" allowfullscreen></iframe>
+</div>
+                         
                          <iframe src="http://51.79.69.16/dhl/getTracking.html"
 
                                 width="600" height="400" scrolling="auto">
@@ -28,8 +9,4 @@
                                  Texto alternativo para navegadores que no aceptan iframes
 
                          </iframe>
-     </div>
-     
-  </div>
-  
-
+   
