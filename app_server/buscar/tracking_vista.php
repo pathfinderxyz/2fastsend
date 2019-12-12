9cabb7f4379c8e4ce7a0ea6ea13f319a115f5dae
pathfinderxyz <josemcarvajal4@gmail.com>
2019-12-12 11:36:19 -0400

    cambio 1

--- app_server/buscar/tracking_vista.php
+++ app_server/buscar/tracking_vista.php
@@ -1,4 +1,4 @@
 <div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="http://51.79.69.16/dhl/getTracking.html" allowfullscreen></iframe>
+  <iframe class="embed-responsive-item" src="51.79.69.16/dhl/getTracking.html" allowfullscreen></iframe>
 </div>
                          
