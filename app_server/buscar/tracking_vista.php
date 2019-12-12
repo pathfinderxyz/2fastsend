51f885efac7006b017063a0bba973961fb76b819
pathfinderxyz <josemcarvajal4@gmail.com>
2019-12-12 11:20:16 -0400

    cambio
    cambio

--- app_server/buscar/tracking_vista.php
+++ app_server/buscar/tracking_vista.php
@@ -1,12 +1,4 @@
 <div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/v64KOxKVLVg" allowfullscreen></iframe>
+  <iframe class="embed-responsive-item" src="https://51.79.69.16/dhl/getTracking.html" allowfullscreen></iframe>
 </div>
                          
-                         <iframe src="http://51.79.69.16/dhl/getTracking.html"
-
-                                width="600" height="400" scrolling="auto">
-
-                                 Texto alternativo para navegadores que no aceptan iframes
-
-                         </iframe>
-   
